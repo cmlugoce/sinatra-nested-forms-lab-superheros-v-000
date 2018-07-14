485ff84862b709d5bd27305a44cfe5e92a0d7b75
Cristina Lugo <cmlugoce@syr.edu>
2018-07-14 20:37:20 +0000

    automatically backed up by learn

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,19 @@
 class Member 
   
+  attr_accessor :name, :bio, :power 
+  
+  @@members
+  
+  def initialize(name, bio, power)
+    @name = name 
+    @bio = bio 
+    @power = power 
+    @@members << self 
+  end 
+  
+  def self.all
+    @@members
+  end 
   
   
 end 
