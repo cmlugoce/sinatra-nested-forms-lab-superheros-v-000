a4cb637d4d29a8d963ab8e1f300c06f86d7c2b16
Cristina Lugo <cmlugoce@syr.edu>
2018-07-14 20:38:57 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,7 +2,15 @@ class Team
   
   attr_accessor :name, :motto
   
-  @@eams = []
+  @@teams = []
   
+  def initialize(name, motto)
+    @name = name 
+    @motto = motto
+    @@teams << self
+  end 
   
+  def self.all 
+    @@teams 
+  end
 end 
